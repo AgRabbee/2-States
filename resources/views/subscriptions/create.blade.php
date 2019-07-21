98d52f22b73ae1ca96e86801ff1a8dc87def6a9c
pavel <zamanbd002@gmail.com>
2019-07-22 00:18:10 +0600

    add create.blade file

---
+++ resources/views/subscriptions/create.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+
+        <form class="col-md-8 m-auto" action="{{ url('/subscriptions/create') }}" method="post">
+            <h4>Your Desire Booty Box</h4>
+            <hr>
+            <div class="form-group row pt-2">
+                <label for="box_name" class="col-sm-3 col-form-label">Booty Box Name</label>
+                <div class="col-sm-9">
+                    <input class="form-control" type="text" id="box_name" placeholder="{{ $box->name }}" readonly>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="price" class="col-sm-3 col-form-label">Price</label>
+                <div class="col-sm-9">
+                    <input class="form-control" type="text" id="price" placeholder="{{ $box->price }}" readonly>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-3 col-form-label">Choose your delivery method</label>
+                <br>
+                <div class="col-sm-9">
+                    @if (count($methods)>0)
+                        @foreach ($methods as $method)
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="btn{{$method->id}}" value="{{$method->id}}">
+                                <label class="form-check-label" for="btn{{$method->id}}">{{ $method->method_name }}</label>
+                            </div>
+                        @endforeach
+                    @endif
+                </div>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary btn-sm" name="submit" value="Subscribe">
+            </div>
+
+
+        </form>
+    </div>
+@endsection
